11148ff2907b6697c8b338bc21ffda67aa911a23
Cesar Sampaio Gomes <cesar.gomes@mstech.com.br>
2022-08-26 17:04:38 -0300

    Project finalized

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,12 +1,13 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
@@ -18,28 +19,51 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        while (true)
+        List<ChessPiece> captured = new ArrayList<>();
+        while (!chessMatch.getCheckMate())
         {
             try
             {
                 UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
+
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
-                ChessPiece capturedPiece = chessMatch.performeChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+                if(capturedPiece != null)
+                    captured.add(capturedPiece);
+
+                if(chessMatch.getPromoted() != null)
+                {
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine().toUpperCase();
+
+                    while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q"))
+                    {
+                        System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+
+                    chessMatch.replacePromotedPiece(type);
+                }
             }
-            catch (ChessException ex)
+            catch (ChessException | InputMismatchException ex)
             {
                 System.out.println(ex.getMessage());
                 sc.nextLine();
             }
         }
-
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
